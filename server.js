60288425ed45966018778d67b29d063d7914bc50
Piyush-Kumar-Tailor <piyush89b2@gmail.com>
2025-04-25 22:27:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const serveIndex = require("serve-index");
 const path = require("path");
+
 const app = express();
 const port = 3000;
 
 const songsDir = path.join(__dirname, "songs");
 
-// Middleware to decode URL-encoded paths
 app.use((req, res, next) => {
   req.url = decodeURIComponent(req.url);
   next();
 });
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.use("/songs", express.static(songsDir), serveIndex(songsDir, { icons: true }));
 
 app.listen(port, () => {
